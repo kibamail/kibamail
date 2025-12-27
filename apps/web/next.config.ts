bb1a632b8415456071b5926d207d3fec88569e25
Frantz Kati <hey@katifrantz.com>
2025-12-27 13:52:32 +0100

    chore: regenerate openapi

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,6 +6,7 @@ import "./env/schema";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  assetPrefix: "/_kibamail-control-plane",
   reactCompiler: false,
   images: {
     remotePatterns: [
