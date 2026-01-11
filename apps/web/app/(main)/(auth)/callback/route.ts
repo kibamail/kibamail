465e5a0a5354b473d79f0bf359f234bbe5eb8100
Frantz Kati <hey@katifrantz.com>
2026-01-11 09:37:28 +0100

    fix: org not showing up after sign up

--- apps/web/app/(main)/(auth)/callback/route.ts
+++ apps/web/app/(main)/(auth)/callback/route.ts
@@ -1,8 +1,13 @@
-import { handleSignIn } from "@logto/next/server-actions";
+import { getLogtoContext, handleSignIn } from "@logto/next/server-actions";
 import { type NextRequest, NextResponse } from "next/server";
 import { logtoConfig } from "@/config/logto";
 import { CookieKey, Cookies } from "@/lib/cookies";
 import { getBaseUrl, normalizePath } from "@/lib/url";
+import {
+  getUserWithOrganizations,
+  invalidateUserCache,
+} from "@/lib/auth/user-cache";
+import { createWorkspaceViaLogto } from "../../api/internal/v1/workspaces/handler";
 
 export async function GET(request: NextRequest) {
   await handleSignIn(logtoConfig, request.nextUrl.searchParams);
@@ -10,6 +15,22 @@ export async function GET(request: NextRequest) {
   const baseUrl = getBaseUrl(request);
   const intended = await Cookies.get(CookieKey.ROUTE_INTENDED);
   const path = normalizePath(intended ?? "");
+
+  const user = await getLogtoContext(logtoConfig);
+
+  if (user?.claims?.sub) {
+    const orgs = await getUserWithOrganizations(user?.claims?.sub);
+
+    if (orgs.organizations.length === 0) {
+      await createWorkspaceViaLogto(
+        {
+          name: user?.claims?.email as string,
+        },
+        user?.claims?.sub
+      );
+    }
+    await invalidateUserCache(user?.claims?.sub);
+  }
 
   return NextResponse.redirect(`${baseUrl}${path}`);
 }
--- apps/web/app/(main)/(dashboard)/w/layout.tsx
+++ apps/web/app/(main)/(dashboard)/w/layout.tsx
@@ -1,5 +1,4 @@
 import type { PropsWithChildren } from "react";
-import { createDefaultWorkspaceAction } from "@/app/(main)/(auth)/actions/create-default-workspace";
 import { getSession } from "@/lib/auth/get-session";
 import { UserProvider } from "@/lib/contexts/user-context";
 import { QueryProvider } from "@/lib/providers/query-provider";
@@ -8,12 +7,7 @@ export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export default async function WorkspaceLayout({ children }: PropsWithChildren) {
-  let session = await getSession();
-
-  if (!session.currentOrganization) {
-    await createDefaultWorkspaceAction();
-    session = await getSession();
-  }
+  const session = await getSession();
 
   return (
     <QueryProvider>
